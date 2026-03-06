64001949cb8ad0f462bfd2385c29115ab02fa0f7
seethygerald <seethygerald@gmail.com>
2026-03-06 11:48:32 +0000

    4

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request
+import joblib
+
+model = joblib.load("foodexp.pkl")
 
 app = Flask(__name__)
 
@@ -21,6 +24,16 @@ def correct():
 @app.route('/wrong', methods=['GET', 'POST'])
 def wrong():
     return(render_template('wrong.html'))
+
+@app.route('/econ', methods=['GET', 'POST'])
+def econ():
+    return(render_template('econ.html'))
+
+@app.route('/foodExp', methods=['GET', 'POST'])
+def foodExp():
+    q = float(request.form.get("q"))
+    r = model.predict([[q]])
+    return(render_template("foodExp.html",r=r[0][0]))
 
 if __name__ == "__main__":
     app.run()
